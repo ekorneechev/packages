b62709d459c863dfd3d54813d4b03af2ba4b2cbf
Korneechev Evgeniy <ekorneechev@gmail.com>
2017-09-26 17:23:52 +0300

    Added script for update localrepo

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+REPO_DIR="/var/ftp/local"
+REPO_NAME="zimbra-foss"
+
+sudo find . -name "*64.rpm" -exec cp {} $REPO_DIR/x86_64/RPMS.$REPO_NAME/ \;
+
+for ARCH in x86_64-i586 x86_64 noarch; do
+    sudo genbasedir --bloat --progress --topdir=$REPO_DIR $ARCH $REPO_NAME
+done
+
+sudo apt-get update
+
