e5f77861058a123f639c6d7e83a774a6d0197144
Korneechev Evgeniy <ekorneechev@gmail.com>
2017-09-26 21:21:09 +0300

    update.sh: removed *-debuginfo

--- update.sh
+++ update.sh
@@ -4,6 +4,8 @@ REPO_NAME="zimbra-foss"
 
 sudo find . -name "*64.rpm" -exec cp {} $REPO_DIR/x86_64/RPMS.$REPO_NAME/ \;
 
+sudo rm -rf $REPO_DIR/x86_64/RPMS.$REPO_NAME/*debuginfo*
+
 for ARCH in x86_64-i586 x86_64 noarch; do
     sudo genbasedir --bloat --progress --topdir=$REPO_DIR $ARCH $REPO_NAME
 done
