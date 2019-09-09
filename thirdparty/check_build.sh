f4c99045308684da189c8c86ac424e1a5509d9ae
Evgeniy Korneechev <ekorneechev@gmail.com>
2019-09-09 17:15:45 +0300

    Update check

--- thirdparty/check_build.sh
+++ thirdparty/check_build.sh
@@ -4,8 +4,9 @@ for file in `find * -name "*64.rpm"`
 do
 	dir=${file%%/*}
 	touch $dir/BUILD.OK
+	echo $dir
 done
 
 find . -name BUILD.OK | wc -l
 echo 'Need:'
-ls | grep -v ".sh" | wc -l
+ls | grep -v "check_build.sh" | wc -l
