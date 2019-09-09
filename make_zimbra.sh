7fc87b5033d672c1ee1c603fd2c75c19b6cd2739
Evgeniy Korneechev <ekorneechev@gmail.com>
2019-09-09 12:46:58 +0300

    Update make_zimbra.sh

--- make_zimbra.sh
+++ make_zimbra.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 cd zimbra/
-for dir in `ls` 
+for dir in `ls | grep -v ".sh"`
 do
+    if [ ! -f "$dir/BUILD.OK" ]; then
+        echo "-----------------------------START-----------------------------"
+	echo $dir
+	echo
 	cd $PWD/$dir; make; cd ..
+	echo "-----------------------------FINISH----------------------------"
+	echo
+	echo
+	echo
+    fi
 done
